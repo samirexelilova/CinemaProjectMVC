f897d2852b622dea257d153c105cdc79bd99ab57
Samire <xsamire2@gmail.com>
2025-06-18 22:06:01 +0400

    Create session and get session

---
+++ StreamitMVC/Areas/Admin/Controllers/SessionController.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using StreamitMVC.DAL;
+using StreamitMVC.Models;
+using StreamitMVC.ViewModels;
+
+namespace StreamitMVC.Areas.Admin.Controllers
+{
+    [Area("admin")]
+    public class SessionController : Controller
+    {
+        private readonly AppDbContext _context;
+        public SessionController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var sessions = _context.Sessions
+                .Include(s => s.Movie)
+                .Include(s => s.Hall)
+                .Include(s => s.Cinema)
+                .Include(s => s.HallPrice)
+                .Include(s => s.Language)
+                .ToList();
+
+            return View(sessions);
+        }
+        public IActionResult Create()
+        {
+            var viewModel = new CreateSessionVM
+            {
+                Movies = _context.Movies.ToList(),
+                Halls = _context.Halls.ToList(),
+                Cinemas = _context.Cinemas.ToList(),
+                HallPrices = _context.HallPrices.ToList(),
+                Languages = _context.Languages.ToList(),
+                Session = new Session()
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateSessionVM viewModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Sessions.Add(viewModel.Session);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            viewModel.Movies = _context.Movies.ToList();
+            viewModel.Halls = _context.Halls.ToList();
+            viewModel.Cinemas = _context.Cinemas.ToList();
+            viewModel.HallPrices = _context.HallPrices.ToList();
+            viewModel.Languages = _context.Languages.ToList();
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ StreamitMVC/ViewModels/SessionVM/CreateSessionVM.cs
@@ -0,0 +1,15 @@
+﻿using StreamitMVC.Models;
+
+namespace StreamitMVC.ViewModels
+{
+    public class CreateSessionVM
+    {
+        public Session Session { get; set; }
+
+        public List<Movie> Movies { get; set; }
+        public List<Hall> Halls { get; set; }
+        public List<Cinema> Cinemas { get; set; }
+        public List<HallPrice> HallPrices { get; set; }
+        public List<Language> Languages { get; set; }
+    }
+}
